41a2628c703b7787f0b2cf1f86c5e1d84ccf0025
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-05-11 18:56:45 -0300

    feat: sample pokemon data

--- PokeBase/Models/Pokemon.swift
+++ PokeBase/Models/Pokemon.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-struct Pokemon {
+struct Pokemon: Hashable {
     let id: UUID
     let name: String
     let imageURL: URL
+}
+
+extension Pokemon {
+    static let exampleData: [Pokemon] = [
+        Pokemon(id: UUID(), name: "Bulbasaur", imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")!),
+        Pokemon(id: UUID(), name: "Charmander", imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png")!),
+        Pokemon(id: UUID(), name: "Squirtle", imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png")!),
+        Pokemon(id: UUID(), name: "Abra", imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/63.png")!),
+        Pokemon(id: UUID(), name: "Lucario", imageURL: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png")!)
+    ]
 }
