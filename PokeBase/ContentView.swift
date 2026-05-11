831c490604ea1fa658ddebb255da3faaa949e739
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-05-11 18:16:50 -0300

    Initial Commit

---
+++ PokeBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeBase
+//
+//  Created by Filipe Fernandes on 11/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeBase/PokeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBaseApp.swift
+//  PokeBase
+//
+//  Created by Filipe Fernandes on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
