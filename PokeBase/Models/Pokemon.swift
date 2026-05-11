f357fd7c5e74996e9a4c415db11b6a3d4d84d84d
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-05-11 18:43:56 -0300

    feat: crated initial Pokemon Model

---
+++ PokeBase/Models/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  PokeBase
+//
+//  Created by Filipe Fernandes on 11/05/26.
+//
+
+import Foundation
+
+struct Pokemon {
+    let id: UUID
+    let name: String
+    let imageURL: URL
+}
