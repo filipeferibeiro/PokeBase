e6e2667d869d805c2d3243f5a6605471cd03b17e
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-05-12 09:32:27 -0300

    feat: fetch data from PokeApi to ListView

--- PokeBase/Views/Screens/PokemonListView.swift
+++ PokeBase/Views/Screens/PokemonListView.swift
@@ -8,20 +8,40 @@
 import SwiftUI
 
 struct PokemonListView: View {
-    @State private var pokemons = Pokemon.exampleData
+    @State private var viewModel = PokemonViewModel()
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(pokemons) { pokemon in
-                    NavigationLink(value: pokemon) {
-                        PokemonCellView(pokemon: pokemon)
+            Group {
+                if viewModel.isLoading && viewModel.pokemons.isEmpty {
+                    ProgressView("Loading Pokémons")
+                        .controlSize(.large)
+                } else if let error = viewModel.errorMessage {
+                    ContentUnavailableView {
+                        Label("Connection error", systemImage: "wifi.exclamationmark")
+                    } description: {
+                        Text(error)
+                    } actions: {
+                        Button("Try again") {
+                            Task { await viewModel.fetchPokemons() }
+                        }
+                    }
+                } else {
+                    List {
+                        ForEach(viewModel.pokemons) { pokemon in
+                            NavigationLink(value: pokemon) {
+                                PokemonCellView(pokemon: pokemon)
+                            }
+                        }
                     }
                 }
             }
             .navigationTitle("Pokémons")
             .navigationDestination(for: Pokemon.self) { selectedPokemon in
                 PokemonDetailView(pokemon: selectedPokemon)
+            }
+            .task {
+                await viewModel.fetchPokemons()
             }
         }
     }
