66a01de4b74c7554db929e3e009045735799d7e8
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-05-12 09:14:03 -0300

    feat: created pokemon response model and get pokemon sprite

--- PokeBase/Models/PokemonResponse.swift
+++ PokeBase/Models/PokemonResponse.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct PokemonResponse: Codable {
+    let count: Int
+    let next: String?
+    let results: [PokemonResult]
+}
+
+struct PokemonResult: Codable {
+    let name: String
+    let url: String
+    
+    var asPokemon: Pokemon? {
+        if let idString = url.split(separator: "/").last, let id = Int(idString) {
+            let spriteURLString = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png"
+            
+            guard let imageURL = URL(string: spriteURLString) else { return nil }
+            
+            return Pokemon(id: id, name: name, imageURL: imageURL)
+        }
+        return nil
+    }
+}
